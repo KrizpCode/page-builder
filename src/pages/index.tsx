642fcafd871ede9c9cb5b233a9285bb7b3f14ff5
j-eriksson <johan.eriksson@barnebys.com>
2022-11-21 21:15:10 +0100

    Added a simple login/logout flow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
 import { type NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
-  return <>Hello</>;
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <>Loading...</>;
+  }
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      {session ? (
+        <button
+          className="rounded-full bg-violet-700 px-4 py-2 text-white"
+          onClick={() => signOut()}
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          className="rounded-full bg-violet-700 px-4 py-2 text-white"
+          onClick={() => signIn("discord")}
+        >
+          Sign In
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default Home;
